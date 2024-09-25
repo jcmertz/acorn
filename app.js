56a673b308c7b94a50dffa2aca73d5af5a58e279
jcmertz <joe@joemertz.com>
2024-09-25 04:02:20 -0400

    More bad ideas again 5

--- app.js
+++ app.js
@@ -8,8 +8,33 @@ const client = next({ dev });
 const handle = client.getRequestHandler();
 const port = 2554;
 
+const passport = require('passport');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
 client.prepare().then(() => {
   const app = express();
+
+  // Configure session middleware
+  const store = new MongoDBStore({
+    uri: process.env.DBHOST,
+    collection: 'userSessions'
+  });
+
+  store.on('error', function(error) {
+    console.log(error);
+  });
+
+  app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  }));
+
+  // Initialize Passport and restore authentication state from session
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   var authRouter = require('./routes/auth');
   var calendarRouter = require('./routes/calendarData');
