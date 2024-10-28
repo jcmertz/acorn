fec1d15857ebb87b98fadbc4fd1d9cdc9bea7a6d
jcmertz <joe+shelob@joemertz.com>
2024-10-27 23:46:29 -0500

    show status updates

--- routes/admin.js
+++ routes/admin.js
@@ -12,6 +12,7 @@ var ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
 var ensureLoggedIn = ensureLogIn();
 
 var url = require("url");
+const { loadGetInitialProps } = require('next/dist/shared/lib/utils.js');
 
 module.exports = function(io) {
     
@@ -32,10 +33,24 @@ module.exports = function(io) {
         res.redirect(req.get("Referrer") || "/");
     });
     
+    const adjustToCentralTime = (date) => {
+        if (!date) return null;
+        // Create a date object
+        let adjustedDate = new Date(date);
+        // Adjust to 8 PM Central Time (US Central Time is UTC-6 or UTC-5 depending on daylight saving)
+        adjustedDate.setUTCHours(20 - adjustedDate.getTimezoneOffset() / 60, 0, 0, 0); // Set to 8:00 PM
+        return adjustedDate;
+    };
+    
+    
     router.post('/updateShowDetail', async (req, res) => {
-        const { id, field, value } = req.body;
+        let { id, field, value } = req.body;
         
         console.log(field + ":" + value);
+        
+        if(field === "showDate") {
+            value = adjustToCentralTime(value);
+        }
         
         // Use computed property to set the key dynamically
         const update = { [field]: value };
